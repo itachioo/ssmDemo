57c308dc6651241e263c06e168d5577e0b65b130
itachioo <1241040660@qq.com>
2019-04-25 22:15:51 +0800

    dao code and test

---
+++ src/test/java/cn/code/dao/SeckillDaoTest.java
@@ -0,0 +1,28 @@
+package cn.code.dao;
+
+import cn.code.entity.Seckill;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+    //注入Dao实现类依赖
+    @Resource
+    private SeckillDao seckillDao;
+
+    @Test
+    public void queryById() throws Exception {
+        long seckillId=1000;
+        Seckill seckill=seckillDao.queryById(seckillId);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+    }
+
+}
